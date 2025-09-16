6fab6de6a04fc567296608248fdac4a9f5e363ed
Satyam Oza <satyamoza@outlook.com>
2025-09-16 03:41:36 -0600

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.js
@@ -0,0 +1,9 @@
+// Last updated: 9/16/2025, 3:11:33 PM
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
