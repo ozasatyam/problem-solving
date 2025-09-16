3e6dec07ee34eed053bd524bc691e562060e7605
Satyam Oza <satyamoza@outlook.com>
2025-09-16 03:39:46 -0600

    Create 3455-MinimumLengthOfStringAfterOperations

---
+++ 3455-MinimumLengthOfStringAfterOperations/3455-MinimumLengthOfStringAfterOperations.js
@@ -0,0 +1,21 @@
+// Last updated: 9/16/2025, 3:09:43 PM
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumLength = function (s) {
+     let charFrequency = new Array(26).fill(0);
+    let totalLength = 0;
+    for (let char of s) {
+        charFrequency[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    for (let frequency of charFrequency) {
+        if (frequency === 0) continue;
+        if (frequency % 2 === 0) {
+            totalLength += 2;
+        } else {
+            totalLength += 1;
+        }
+    }
+    return totalLength;
+};
