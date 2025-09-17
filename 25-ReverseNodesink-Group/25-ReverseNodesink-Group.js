4d85e7c0dc897b1313c5b17e4e2a8311b0ec5eb1
Satyam Oza <satyamoza@outlook.com>
2025-09-17 10:21:25 -0600

    Create 25-ReverseNodesink-Group

---
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.js
@@ -0,0 +1,34 @@
+// Last updated: 9/17/2025, 9:51:22 PM
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function (head, k) {
+    let newNode = head
+    for (let i = 0; i < k; i++) {
+        if (!newNode) return head
+        newNode = newNode.next
+
+    }
+
+    let nextNode = reverseKGroup(newNode, k)
+    let temp = head
+    let m = k
+    while (m > 0) {
+        m--;
+        let next = temp.next
+        temp.next = nextNode
+        nextNode = temp
+        temp = next
+    }
+    return nextNode
+
+};
