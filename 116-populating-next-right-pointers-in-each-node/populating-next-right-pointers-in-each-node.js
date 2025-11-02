2c78c59a99e2a5d94841daf52fc8770bd0dbdc1b
Satyam Oza <satyamoza@outlook.com>
2025-11-02 11:23:14 +0530

    Time: 38 ms (99.44%) | Memory: 60.5 MB (56.98%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
@@ -12,28 +12,27 @@
  * @param {_Node} root
  * @return {_Node}
  */
-var connect = function(root) {
-    if (!root) return null;
-    
-    let leftmost = root;
-    
-    while (leftmost && leftmost.left) {
-        let curr = leftmost;
-        
-        while (curr) {
-            // Connect left child to right child
-            curr.left.next = curr.right;
-            
-            // Connect right child to next node's left child
-            if (curr.next) {
-                curr.right.next = curr.next.left;
-            }
-            
-            curr = curr.next;
+var connect = function (root) {
+    if (!root) return null; //
+
+    let curr = root;
+    let next = root.left;
+
+    while (curr && next) {
+        curr.left.next = curr?.right;
+
+        if (curr.next) {
+            curr.right.next = curr.next.left;
         }
-        
-        leftmost = leftmost.left;
+
+        curr = curr.next;
+
+        if (!curr) {
+            curr = next;
+            next = curr.left 
+        }
+       
     }
-    
+
     return root;
 };
