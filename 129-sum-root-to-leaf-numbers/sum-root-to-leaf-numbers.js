947e4c6cf68a959485de498ea8ccb2d82eea23a5
Satyam Oza <satyamoza@outlook.com>
2025-11-12 18:06:14 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (35.06%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root) {
+    const array = new Array()
+    function dfs(root, path) {
+        if (!root) return null
+        path *= 10
+        path += root.val
+        if (!root.left && !root.right) return array.push(path)
+        dfs(root.left, path)
+        dfs(root.right, path)
+    }
+    dfs(root, 0)
+
+
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+};
