0f7a068db0e1bed98c0bfd3725884042d75f9454
Satyam Oza <satyamoza@outlook.com>
2025-12-21 01:21:22 +0530

    Time: 8 ms (3.54%) | Memory: 58.1 MB (5.80%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+    let zigzag = 1
+    let array = (root !== null) ? [root] : []
+    console.log(array, "array")
+    let ans = []
+    while (array.length) {
+        let n = array.length
+        let arr = []
+        for (let i = 0; i < n; i++) {
+            const node = array.shift()
+            if (node?.val !== undefined) {
+                if ((zigzag % 2) == 0) {
+                    arr.unshift(node.val)
+
+                } else {
+                    arr.push(node.val)
+                }
+                node?.left && array.push(node.left)
+                node?.right && array.push(node.right)
+            }
+        }
+        ans.push(arr)
+        zigzag++
+    }
+    return ans
+};
